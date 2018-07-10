477557952f880d61adebafb1906a0a6b844a537d
Caroline Liongosari <CarolineLiong@comcast.net>
2018-07-10 13:30:32 -0700

    redid AppBar with old Material-ui version

--- src/components/nav_bar.js
+++ src/components/nav_bar.js
@@ -1,23 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Component } from 'react';
-import AppBar from '@material-ui/core/AppBar'
-import Toolbar from '@material-ui/core/Toolbar'
-import Typography from '@material-ui/core/Typography'
+import AppBar from 'material-ui/AppBar'
 
 
-const NavBar = () => {
-  return (
-      <div>
-        <AppBar className= "app-bar" position="static">
-          <Toolbar>
-            <Typography variant="title" color="inherit">
-              Impekable
-            </Typography>
-          </Toolbar>
-        </AppBar>
-      </div>
-  );
-};
+const NavBar = () => (
+  <AppBar
+  title="Impekable"
+  iconClassNameRight="muidocs-icon-navigation-expand-more"
+  />
+);
+
 
 export default NavBar;
