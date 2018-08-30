29a4c526186dbcc79effe62b800f02f901ce1478
Caroline Liongosari <CarolineLiong@comcast.net>
2018-08-30 13:10:06 -0700

    added documentation

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 import NavBar from '../components/nav_bar';
 
+/**
+ * The App component returns the Navigation bar component
+ */
 const App = () => <div> <NavBar /> </div>;
 
 export default App;
